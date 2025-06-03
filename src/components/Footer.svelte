18e839a4a2ac9e767e1c943274a7510a60824d3e
Gabriel Nordeborn <gabbe.nord@gmail.com>
2025-06-03 21:23:51 +0200

    extend example

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,15 @@
 <script lang="typescript">
   let year = 2025;
+  let names = ["John", "Jane", "Jim", "Jill"];
 </script>
 
 <div>Hello! It's year {year}</div>
+
+<div>
+  <h3>Names:</h3>
+  <ul>
+    {#each names as name}
+      <li>{name}</li>
+    {/each}
+  </ul>
+</div>
