22e1bfe3b145a5cb0e243e016c09da80ef5293d6
Gabriel Nordeborn <gabbe.nord@gmail.com>
2025-06-03 21:23:01 +0200

    initial commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,5 @@
+<script lang="typescript">
+  let year = 2025;
+</script>
+
+<div>Hello! It's year {year}</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="typescript">
+  import Footer from "../components/Footer.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<Footer />
